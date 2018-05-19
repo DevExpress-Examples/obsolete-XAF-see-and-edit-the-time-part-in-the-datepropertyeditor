4532bdef364839e285b1bbe54c58123d2e52339c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ VB/E1497VB/WinSolution.Module/DomainObject1.vb
@@ -0,0 +1,26 @@
+Imports DevExpress.ExpressApp.Model
+Imports Microsoft.VisualBasic
+Imports DevExpress.Xpo
+Imports DevExpress.Persistent.Base
+Imports DevExpress.Persistent.BaseImpl
+Imports System
+
+Namespace WinSolution.Module
+	<DefaultClassOptions> _
+	Public Class DomainObject1
+		Inherits BaseObject
+		Public Sub New(ByVal session As Session)
+			MyBase.New(session)
+		End Sub
+		Private _createdOn As DateTime
+		<ModelDefault("DisplayFormat", "{0: ddd, dd MMMM yyyy hh:mm:ss tt}"), ModelDefault("EditMask", "ddd, dd MMMM yyyy hh:mm:ss tt"), ModelDefault("PropertyEditorType", "WinSolution.Module.Win.MyDatePropertyEditor")> _
+		Public Property CreatedOn() As DateTime
+			Get
+				Return _createdOn
+			End Get
+			Set(ByVal value As DateTime)
+				SetPropertyValue("CreatedOn", _createdOn, value)
+			End Set
+		End Property
+	End Class
+End Namespace
